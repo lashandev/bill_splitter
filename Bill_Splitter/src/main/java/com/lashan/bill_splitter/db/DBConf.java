479f4d073d31d7e4ba751375295680198845c6ec
lashandev <lashandev@gmail.com>
2024-01-29 01:17:44 +0530

    Initial Commit

---
+++ Bill_Splitter/src/main/java/com/lashan/bill_splitter/db/DBConf.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lashan.bill_splitter.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Lashan
+ */
+public class DBConf {
+
+    private static Connection connection;
+
+    private static void createConnection() throws Exception {
+        String url = "jdbc:sqlite:H:\\Project_Dir\\Lash_APP_Test\\app_db\\fun_split";
+        connection = DriverManager.getConnection(url);
+        System.out.println("Connection to SQLite has been established.");
+    }
+    
+    public static void iud(String sql)throws Exception{
+        if (connection == null) {
+            createConnection();
+        }
+        connection.createStatement().executeUpdate(sql);
+    }
+    
+    public static ResultSet search(String sql)throws Exception{
+        if (connection == null) {
+            createConnection();
+        }
+        return connection.createStatement().executeQuery(sql);
+    }
+    
+    public static Connection getConnection(String sql)throws Exception{
+        if (connection == null) {
+            createConnection();
+        }
+        return connection;
+    }
+    
+    
+
+}
